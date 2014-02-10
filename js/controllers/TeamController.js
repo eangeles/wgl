4b22ff279b6959b28161c762f2dc812637331bba
Michael Fleming <mfleming7024@gmail.com>
2014-02-10 18:10:18 -0500

    Added team controller

---
+++ js/controllers/TeamController.js
@@ -0,0 +1,32 @@
+wgl.controller('teams', ['$scope','$routeParams','$location','$rootScope','$firebase', function mtCtrl($scope, $routeParams, $location, $rootScope, $firebase) {
+
+    var teamsURL = "https://thewgl.firebaseio.com/thewgl/teams/";
+    $scope.teams = $firebase(new Firebase(newsURL));
+    
+    $scope.addTeam = function(team) {                
+        $scope.teams.$add(team);
+        $location.path("/teams");
+    };
+
+    $scope.removeTeam = function(teamID) {
+        $scope.teams.$remove(teamID);
+    }
+    
+    var updateRef = "";
+    $scope.updateTeam = function(post) {
+        updateRef = new Firebase("https://thewgl.firebaseio.com/thewgl/teams/" + $routeParams.teamID);
+        updateRef.update({
+            //Update Content
+            //1. Array of members
+            //2. Team Name
+            //3. Team Picture
+            //4. Upcoming matches?
+        });
+        $location.path("/teams");
+    }
+    
+    $scope.retrieveTeamInfo = function() {
+        $scope.selectedTeam = $firebase(new Firebase("https://thewgl.firebaseio.com/thewgl/teams/" + $routeParams.teamID));
+    }
+
+}]);
