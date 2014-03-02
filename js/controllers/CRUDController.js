5f1bd0be45a6f0969bb6cb907c98553108346d01
eangeles <niwre2007@gmail.com>
2014-03-02 01:19:09 -0500

    merge

--- js/controllers/CRUDController.js
+++ js/controllers/CRUDController.js
@@ -68,13 +68,9 @@ wgl.controller('gameController', ['$scope','$firebase','$location', function($sc
         $scope.gameInfos.gameSystem = game.gameSystem;
         $scope.gameInfos.gameArtUrl = game.gameArtUrl;
         $scope.gameInfos.gameQuantity = game.gameQuantity;
-        $scope.gameInfos.id = gameId;
-        console.log(gameId);
+//        $scope.gameInfos.id = gameId;
+//        console.log(gameId);
         $scope.typing = false;
-<<<<<<< HEAD
-
-=======
->>>>>>> 7bf8f8e36d12a069293e569c6891e3b5c0b73c34
     };
 }]);
 
