c0432eddb9172e703e648df98a56a3657801e976
Michael Fleming <mfleming7024@gmail.com>
2014-03-14 18:43:33 -0400

    adding new tournament features

--- js/controllers/TournamentController.js
+++ js/controllers/TournamentController.js
@@ -7,46 +7,159 @@ wgl.controller('tournaments', ['$scope','$routeParams','$location','$rootScope',
     $scope.addTournament = function(tournament) {
         tournament.firstRound = [
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             },
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             },
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             },
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             }
         ];
         tournament.secondRound = [
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             },
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             }
         ];
         tournament.thirdRound = [
             {
-                homeTeam: {name: "Placeholder Home Team"},
-                awayTeam: {name: "Placeholder Away Team"},
+                homeTeam: {
+                    name: "Placeholder Home Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
+                awayTeam: {
+                    name: "Placeholder Away Team",
+                    bio: "",
+                    wins: 0,
+                    losses: 0,
+                    picture: "",
+                    key: "",
+                    players: {}
+                },
                 date: "01/02/2014"
             }
         ];
         console.log(tournament);
+        $scope.tournaments.$add(tournament);
         $location.path("/tournaments");
     };
 
@@ -75,8 +188,17 @@ wgl.controller('tournaments', ['$scope','$routeParams','$location','$rootScope',
     $scope.selectedTournamentMatch = $firebase(tournyMatchRef);
 
     $scope.updateMatch = function(match) {
-        console.log(match);
-        tournyMatchRef.update(match);
+        tournyMatchRef.update({
+                homeTeam: {
+                    name:       match.homeTeam.name,
+                    picture:    match.homeTeam.picture
+                },
+                date: match.date,
+                awayTeam: {
+                    name:       match.awayTeam.name,
+                    picture:    match.awayTeam.picture 
+                }
+        });
         $location.path("/tournaments");
     }
 
@@ -85,17 +207,15 @@ wgl.controller('tournaments', ['$scope','$routeParams','$location','$rootScope',
     $scope.awayTeamTyping = false;
     //Filter user search and select to input
     $scope.updateTeamAway = function(team) {
-        $scope.selectedTournamentMatch.awayTeam.name = team.name;
-        team.key = team.$key;
-        $scope.selectedTournamentMatch.awayTeam = team;
+        $scope.selectedTournamentMatch.awayTeam.name = angular.fromJson(angular.toJson(team.name));
+        $scope.selectedTournamentMatch.awayTeam = angular.fromJson(angular.toJson(team));
         $scope.awayTeamTyping = false;
     };
 
     $scope.homeTeamTyping = false;
     $scope.updateTeamHome = function(team) {
-        $scope.selectedTournamentMatch.homeTeam.name = team.name;
-        team.key = team.$key;
-        $scope.selectedTournamentMatch.homeTeam = team;
+        $scope.selectedTournamentMatch.homeTeam.name = angular.fromJson(angular.toJson(team.name));
+        $scope.selectedTournamentMatch.homeTeam = angular.fromJson(angular.toJson(team));
         $scope.homeTeamTyping = false;
     };
 
